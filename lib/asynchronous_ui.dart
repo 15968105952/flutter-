cbe25da061e1ffca03cd90430150d9392dc92b4d
15968105952 <1824902907@qq.com>
2020-05-25 17:36:36 +0800

    多数据时请求

--- lib/asynchronous_ui.dart
+++ lib/asynchronous_ui.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:dio/dio.dart';
 
 
 class AsynchronousUI extends StatefulWidget {
@@ -16,7 +17,7 @@ class _AsynchronousUIState extends State<AsynchronousUI> {
   @override
   void initState() {
     super.initState();
-
+    //请求网络数据
     loadData();
   }
   @override
@@ -37,14 +38,17 @@ class _AsynchronousUIState extends State<AsynchronousUI> {
 
   Widget getRow(int i) {
     return new Padding(padding: new EdgeInsets.all(10.0),
-      child: new Text("Row ${widgets[i]["title"]}"),);
+      child: new Text("${widgets[i]["title"]}"));
   }
 
   Future loadData() async {
     String dataURL = "https://jsonplaceholder.typicode.com/posts";
     http.Response response=await http.get(dataURL);
+    Response response2 = await new Dio().get(dataURL);
+    print("widgets:"+response2.toString());
     setState(() {
       widgets = json.decode(response.body);
+      print("widgets:"+response.body.toString());
     });
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutterapp/animation_processing.dart';
-import 'package:flutterapp/canvas_draw_paint.dart';
-import 'package:flutterapp/custom_button.dart';
 import 'package:http/http.dart' as http;
-import 'package:flutterapp/asynchronous_ui.dart';
+import 'dart:async';
+import 'dart:isolate';
 
 void main() => runApp(new SampleApp());
 
@@ -30,13 +30,100 @@ class SampleApp extends StatelessWidget {
 //    home:new Scaffold(
 //      body: new DemoApp(),
 //    )
-    //自定义按钮
+      //自定义按钮
 //    home: new Center(
 //      child: new CustomButton("hello"),
 //    ),
-    //异步UI
-      home: new AsynchronousUI(),
+      //异步UI
+      home: new IsolateUI(),
     );
   }
 }
 
+class IsolateUI extends StatefulWidget {
+  IsolateUI({Key key}) : super(key: key);
+
+  @override
+  _IsolateUIState createState() => new _IsolateUIState();
+}
+
+class _IsolateUIState extends State<IsolateUI> {
+  List widgets = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Sample App"),
+        ),
+        body: getBody());
+  }
+
+  getBody() {
+    if (showLoadingDialog()) {
+      return getProgressDialog();
+    } else {
+      return getListView();
+    }
+  }
+
+  showLoadingDialog() {
+    if (widgets.length == 0) {
+      return true;
+    }
+
+    return false;
+  }
+
+  getProgressDialog() {
+    return new Center(child: new CircularProgressIndicator());
+  }
+
+  ListView getListView() => new ListView.builder(
+      itemCount: widgets.length,
+      itemBuilder: (BuildContext context, int position) {
+        return getRow(position);
+      });
+
+  Widget getRow(int position) {
+    return new Padding(
+        padding: new EdgeInsets.all(10.0),
+        child: new Text("${widgets[position]["title"]}"));
+  }
+
+  void loadData() async {
+    ReceivePort receivePort = new ReceivePort();
+    await Isolate.spawn(dataloader, receivePort.sendPort);
+
+    SendPort sendPort = await receivePort.first;
+
+    List msg = await sendReceive(
+        sendPort, "https://jsonplaceholder.typicode.com/posts");
+  }
+
+  static dataloader(SendPort sendPort) async {
+    ReceivePort receivePort = new ReceivePort();
+    sendPort.send(receivePort.sendPort);
+    await for (var msg in receivePort) {
+      String data = msg[0];
+      SendPort replyTo = msg[1];
+      String dataURL = data;
+      http.Response response = await http.get(dataURL);
+      replyTo.send(json.decode(response.body));
+    }
+  }
+
+  Future sendReceive(SendPort sendPort, String msg) {
+    ReceivePort receivePort = new ReceivePort();
+    sendPort.send([msg, receivePort.sendPort]);
+    return receivePort.first;
+  }
+}
